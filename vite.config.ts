6575654b900b2de2bbb5de4c37046e7538df9413
Charles Junio Gomes dos Santos <cjunniior@gmail.com>
2025-04-14 16:47:15 -0300

    Atualização

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/charleszzard/teste-estagio', // Repositorio
+  base: '/teste-estagio/',
   plugins: [react()],
-});
+})
